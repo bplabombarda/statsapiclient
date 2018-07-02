a0532d4cfb039d9742aee49576f12bc5ded45241
Brett LaBombarda <blabombarda@emmisdigital.com>
2018-07-01 22:31:16 -0500

    Adding Games module.

---
+++ statsapiclient/games.py
@@ -0,0 +1,20 @@
+from statsapiclient import Client
+
+
+class Game(Client):
+	_endpoint = '/api/v1/game/{game_pk}/feed/live'
+
+	def __init__(self, game_pk):
+		self.game_pk = game_pk
+
+	def play_by_play(self):
+		url = self._endpoint.format(game_pk=self.game_pk)
+		
+		response = self.fetch(url, params=None)
+		return response['liveData']['plays']
+
+	def boxscore(self):
+		url = self._endpoint.format(game_pk=self.game_pk)
+		
+		response = self.fetch(url, params=None)
+		return response['liveData']['boxscore']
