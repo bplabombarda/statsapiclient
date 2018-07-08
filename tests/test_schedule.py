24bf25656af162848a7726e27942280b1a9ea867
Brett LaBombarda <bplabombarda@gmail.com>
2018-07-08 16:12:13 -0500

    Fixing test_schedule; Adding Makefile and .travis.yml for CI.

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -6,8 +6,8 @@ from statsapiclient import Schedule
 s = Schedule('2017-10-23')
 g = s.get_games()
 
-gk_one = games[0]['gamePk']
-gk_two = games[1]['gamePk']
+gk_one = g[0]['gamePk']
+gk_two = g[1]['gamePk']
 
 def test_schedule():
 	assert gk_one == 2017020123 and gk_two == 2017020124
