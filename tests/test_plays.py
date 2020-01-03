e1d77ebf9497df0162cb44158052ba6c8d1d08c8
Brett LaBombarda <bplabombarda@gmail.com>
2020-01-02 23:45:52 -0600

    mas testing

--- tests/test_plays.py
+++ tests/test_plays.py
@@ -1,3 +1,5 @@
+import pytest
+
 from statsapiclient.plays import Play
 
 mock_plays = {
@@ -22,8 +24,8 @@ mock_plays_by_period = {
         {"about": {"eventIdx": 5}},
     ],
     "current_play": {"My": "play"},
-    "penalty_indicies": [1, 2, 3],
-    "scoring_indicies": [4, 5, 6],
+    "penalty_indicies": [0, 1, 2],
+    "scoring_indicies": [3, 4, 5],
     "play_indicies_by_period": {
         1: {"startIndex": 0, "endIndex": 1},
         2: {"startIndex": 2, "endIndex": 3},
@@ -53,3 +55,32 @@ class TestPlays:
 
         third = [{"about": {"eventIdx": 4}}, {"about": {"eventIdx": 5}}]
         assert plays.get_plays_by_period(3) == third
+
+    def test_get_plays_by_period_exception(self):
+        plays = Play(**mock_plays_by_period)
+
+        with pytest.raises(ValueError) as excinfo:
+            plays.get_plays_by_period(0)
+        assert "Period value should be 1, 2, 3, or 4" in str(excinfo.value)
+
+        with pytest.raises(ValueError) as excinfo:
+            plays.get_plays_by_period(5)
+        assert "Period value should be 1, 2, 3, or 4" in str(excinfo.value)
+
+    def test_get_penalty_plays(self):
+        plays = Play(**mock_plays_by_period)
+        boo = [
+            {"about": {"eventIdx": 0}},
+            {"about": {"eventIdx": 1}},
+            {"about": {"eventIdx": 2}},
+        ]
+        assert plays.get_penalty_plays() == boo
+
+    def test_get_scoring_plays(self):
+        plays = Play(**mock_plays_by_period)
+        yay = [
+            {"about": {"eventIdx": 3}},
+            {"about": {"eventIdx": 4}},
+            {"about": {"eventIdx": 5}},
+        ]
+        assert plays.get_scoring_plays() == yay
